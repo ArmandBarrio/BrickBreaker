80738a52cf376f07514be37a1bc1eedc59852e06
gmorvillie <gui.morvillier@gmail.com>
2015-12-11 18:49:31 +0100

    rien

--- Brick.java
+++ Brick.java
@@ -1,7 +1,9 @@
 
 public class  Brick extends Object {
-    public Brick(int ax, int ay, String Type, int state){
+    public int state;
+    public Brick(int ax, int ay, String Type, int s){
         super( Type, ax, ay, 0,0);
+        this.state = s;
         
     }
 }
